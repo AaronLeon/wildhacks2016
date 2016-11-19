1f6f863d455d7b22563d413f6a1eb39eaa451b46
Meg Grasse <meggrasse@gmail.com>
2016-11-19 14:09:30 -0600

    Broken camera.

--- GestureControl/GestureControl/ViewController.m
+++ GestureControl/GestureControl/ViewController.m
@@ -9,11 +9,13 @@
 #import "ViewController.h"
 #import "ClarifaiApp.h"
 
-@implementation ViewController 
+@implementation ViewController
 
 typedef void (^ClarifaiPredictionsCompletion)(NSArray <ClarifaiOutput *> *outputs, NSError *error);
 static NSString * const modelID = @"general";
 static NSString * const versionID = @"";
+
+ClarifaiApp *app;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,6 +25,8 @@ static NSString * const versionID = @"";
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(captureStateChanged:) name:AVCaptureSessionDidStartRunningNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(captureStateChanged:) name:AVCaptureSessionDidStopRunningNotification object:nil];
     [self accessCamera];
+    
+    app = [[ClarifaiApp alloc] initWithAppID:@"O-lRr8yaf-2co6U5NvsCekAvL1QlsdXveH5Db8E9" appSecret:@"E0U4dPa1klxionbwfD3reRviXWmSvqjr7M1frCSe"];
 }
 
 
@@ -50,6 +54,7 @@ AVCaptureSession *session;
     dispatch_queue_t queue = dispatch_queue_create("camera", 0);
     [output setSampleBufferDelegate:self queue:queue];
     [session addOutput:output];
+    NSLog(@"video settings: %@", output.videoSettings);
 }
 
 
@@ -58,25 +63,39 @@ AVCaptureSession *session;
     NSLog(@"%@", notification);
 }
 
-- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection;
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer  fromConnection:(AVCaptureConnection *)connection
 {
-    NSData *data = imageToBuffer(sampleBuffer);
-    NSLog(@"%@", data);
+    //... just an example of how to get an image out of this ...
+    
+    CGImageRef cgImage = [self imageFromSampleBuffer:sampleBuffer];
+    if (cgImage) {
+        NSLog(@"works");
+    }
+    CGImageRelease( cgImage );
 }
 
-NSData* imageToBuffer( CMSampleBufferRef source) {
-    CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(source);
-    CVPixelBufferLockBaseAddress(imageBuffer,0);
-
+- (CGImageRef) imageFromSampleBuffer:(CMSampleBufferRef) sampleBuffer // Create a CGImageRef from sample buffer data
+{
+    CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    CVPixelBufferLockBaseAddress(imageBuffer,0);        // Lock the image buffer
+    
+    uint8_t *baseAddress = (uint8_t *)CVPixelBufferGetBaseAddressOfPlane(imageBuffer, 0);   // Get information of the image
     size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer);
+    NSLog(@"size: %zu", bytesPerRow);
     size_t width = CVPixelBufferGetWidth(imageBuffer);
     size_t height = CVPixelBufferGetHeight(imageBuffer);
-    void *src_buff = CVPixelBufferGetBaseAddress(imageBuffer);
-
-    NSData *data = [NSData dataWithBytes:src_buff length:bytesPerRow * height];
-
-    CVPixelBufferUnlockBaseAddress(imageBuffer, 0);
-    return data;
+    NSLog(@"height:, %zu", height);
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    CGContextRef newContext = CGBitmapContextCreate(baseAddress, width, height, 8, 0, colorSpace, kCGImageAlphaNone);
+    CGImageRef newImage = CGBitmapContextCreateImage(newContext);
+    CGContextRelease(newContext);
+    
+    CGColorSpaceRelease(colorSpace);
+    CVPixelBufferUnlockBaseAddress(imageBuffer,0);
+    /* CVBufferRelease(imageBuffer); */  // do not call this!
+    
+    return newImage;
 }
 
 @end
